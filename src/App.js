82306f08e7ea5bfafca3ead55410fd4aac5f6cc1
Nitin Patel <nitinpatel278@gmail.com>
2019-06-22 23:51:31 +0530

    basic layout with ant design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import 'antd/dist/antd.css';
+
+import { Layout, Typography } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+const { Title } = Typography;
 
 const App = () => {
-  return <h1>Hello, World</h1>;
+  return (
+    <Layout className="layout">
+      <Header>
+        <Title style={{ color: '#fff', padding: '15px' }} level={3}>
+          Crypto Portfolio Tracker Arweave
+        </Title>
+      </Header>
+      <Content style={{ padding: '25px 50px' }}>
+        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Crypto Portfolio Tracker Arweave ©2019 Created by Nitin Patel
+      </Footer>
+    </Layout>
+  );
 };
 
 export default App;
